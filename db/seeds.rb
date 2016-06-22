11981ba4fd8f4577432e2612898740d04275933c
João Pedro Evangelista <joaoevangelista@protonmail.com>
2016-06-21 23:49:45 -0300

    Seeds for IncidentType

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,11 @@
 # User seed
 
 User.create!(email: 'john.doe@fireplace.com', password:'123456', name: 'John Doe')
+
+# Incident Types
+
+IncidentType.create!(name: 'Fireplace')
+IncidentType.create!(name: 'Fishing')
+IncidentType.create!(name: 'Fireworks')
+IncidentType.create!(name: 'Hunting')
+IncidentType.create!(name: 'Lost Person')
