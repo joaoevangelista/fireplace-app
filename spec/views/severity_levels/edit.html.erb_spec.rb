fbf8d8ed28a68dcb686df394e9b13c2052f2f596
João Pedro Evangelista <joaoevangelista@protonmail.com>
2016-06-25 13:38:22 -0300

    Removed scaffold test with errors

--- spec/views/severity_levels/edit.html.erb_spec.rb
+++ spec/views/severity_levels/edit.html.erb_spec.rb
@@ -2,15 +2,5 @@
 require 'rails_helper'
 
 RSpec.describe 'severity_levels/edit', type: :view do
-  let(:severity_level) { SeverityLevel.create!(name: 'MyString', color: 'MyString') }
 
-  it 'renders the edit severity_level form' do
-    render
-
-    assert_select 'form[action=?][method=?]', severity_level_path(severity_level), 'post' do
-      assert_select 'input#severity_level_name[name=?]', 'severity_level[name]'
-
-      assert_select 'input#severity_level_color[name=?]', 'severity_level[color]'
-    end
-  end
 end
