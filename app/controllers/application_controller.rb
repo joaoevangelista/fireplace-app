7aef616fb675822fc6291a4bcbead539aa8a3ba0
João Pedro Evangelista <joaoevangelista@protonmail.com>
2016-06-25 18:21:43 -0300

    fix offences

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-
   # Overwriting the sign_out devise redirect path method
-  def after_sign_out_path_for(resource_or_scope)
+  def after_sign_out_path_for(_resource_or_scope)
     root_path
   end
 end
