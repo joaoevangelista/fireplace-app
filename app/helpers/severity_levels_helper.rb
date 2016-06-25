da7b43161735d204d7f8e64d0865c42cbb75d85c
João Pedro Evangelista <joaoevangelista@protonmail.com>
2016-06-25 15:37:22 -0300

    Add severity level labels on show and index page of fire

--- app/helpers/severity_levels_helper.rb
+++ app/helpers/severity_levels_helper.rb
@@ -5,22 +5,23 @@ module SeverityLevelsHelper
   # Render a span label for the given severity
   def severity_label(severity)
     text_color = text_color_for_bg(severity.color)
-    "<span class='label' style='background: #{severity.color} ; color: #{text_color}'>#{severity.name}</span>"
+    "<span class='label' style='background: #{severity.color} ; color: #{text_color}'>
+    #{severity.name}</span>".html_safe
   end
 
   # Find a sutiable color based on the background color
   def text_color_for_bg(bg_color)
-      if bg_color.length == 4
-        r, g, b = bg_color[1, 4].scan(/./).map { |v| (v * 2).hex }
-      else
-        r, g, b = bg_color[1, 7].scan(/.{2}/).map(&:hex)
-      end
-
-      if (r + g + b) > 500
-        '#333333'
-      else
-        '#FFFFFF'
-      end
+    if bg_color.length == 4
+      r, g, b = bg_color[1, 4].scan(/./).map { |v| (v * 2).hex }
+    else
+      r, g, b = bg_color[1, 7].scan(/.{2}/).map(&:hex)
     end
+
+    if (r + g + b) > 500
+      '#333333'
+    else
+      '#FFFFFF'
+    end
+  end
 
 end
