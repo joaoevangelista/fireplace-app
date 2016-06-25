41a4661812c5b98c28bfe68dfb5c76d35d567010
João Pedro Evangelista <joaoevangelista@protonmail.com>
2016-06-25 13:57:39 -0300

    Add helper method to find a color based on background of severity

--- app/helpers/severity_levels_helper.rb
+++ app/helpers/severity_levels_helper.rb
@@ -1,4 +1,20 @@
 # frozen_string_literal: true
 # :nodoc:
 module SeverityLevelsHelper
+  
+  # Find a sutiable color based on the background color
+  def text_color_for_bg(bg_color)
+      if bg_color.length == 4
+        r, g, b = bg_color[1, 4].scan(/./).map { |v| (v * 2).hex }
+      else
+        r, g, b = bg_color[1, 7].scan(/.{2}/).map(&:hex)
+      end
+
+      if (r + g + b) > 500
+        '#333333'
+      else
+        '#FFFFFF'
+      end
+    end
+
 end
