1be1266ac00e2a620175f221d5066a2f229159f6
João Pedro Evangelista <joaoevangelista@protonmail.com>
2016-06-26 12:25:20 -0300

    Fix rubocop offenses for validation, fix #12

--- app/validators/color_validator.rb
+++ app/validators/color_validator.rb
@@ -1,12 +1,12 @@
+# frozen_string_literal: true
 # Based on GitLab's implementation
 # Custom validator for web color codes. It requires the leading hash symbol and
 # will accept RGB triplet or hexadecimal formats.
 class ColorValidator < ActiveModel::EachValidator
-   PATTERN = /\A\#[0-9A-Fa-f]{3}{1,2}+\Z/.freeze
+  PATTERN = /\A\#[0-9A-Fa-f]{3}{1,2}+\Z/
 
-   def validate_each(record, attribute, value)
-     unless value =~ PATTERN
-       record.errors.add(attribute, "must be a valid color code")
-     end
-   end
+  def validate_each(record, attribute, value)
+    return true if value =~ PATTERN # <- guard clause
+    record.errors.add(attribute, 'must be a valid color code')
+  end
 end
