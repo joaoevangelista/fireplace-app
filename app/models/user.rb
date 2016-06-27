42656a8c03d1141621ba8c4759c7fbb4adeba64e
João Pedro Evangelista <joaoevangelista@protonmail.com>
2016-06-27 17:11:14 -0300

    Add user name validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,13 @@
 # :nodoc:
 class User
   include NoBrainer::Document
+
+  validates :name, presence: true
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  :recoverable, :rememberable, :trackable, :validatable
 
   ## Database authenticatable
   field :email,              type: String, default: ''
